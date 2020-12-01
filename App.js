c81a8e1000c8558757f290876d1fc85bbcac3144
IevaUlme <ieva.ulme@va.lv>
2020-12-01 13:15:00 +0200

    Hello world

---
+++ App.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default function App() {
+  return (
+    
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Hello world form group G3: Ieva Ulme.</Text>
+	  <Text>This is my Ievas Ulmes first React Native aplication!
+      </Text>
+    <Image  source={require('./assets/rain.jpg')}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
