92112936a34352058198af9e88f30438bc8e30b5
AleksisVorobjovs <73535402+AleksisVorobjovs@users.noreply.github.com>
2020-12-03 13:15:05 +0200

    Add files via upload

---
+++ components/AssetExample.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/just_an_image1.jpg')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    height: 128,
+    width: 318,
+  }
+});
